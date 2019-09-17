5ccade4aa4ef95e1f6f7614fc719467c1b01ea4e
Ashwin Jacob <math0268@algonquinlive.com>
2019-09-17 11:45:23 -0400

    console output while work

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-var name = 'Ashwin Jacob'
-console.log('hello my name is' + name +)
-var number = 3
+const student = {
+  name: 'jessica',
+  age: 25,
+  isActive: true
+}
+console.log(student)
+
+student.classList = ['English', 'Art', 'Science']
+console.log(student.classList[1])
+
+const story = {
+  noun: 'Toronto',
+  verbs: ['run', 'walk'],
+  adjectives: ['beautiful', 'red']
+
+}
+
+console.log(`The sun did not ${story.verbs[0]}
+It was too ${ story.adjectives[0]} to ${story.verbs[1]} 
+So we sat in the  ${ story.noun}
+All that, ${ story.adjectives[1]}, [adjective 1] day.`)
